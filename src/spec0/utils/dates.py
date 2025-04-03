eef2de45f313ae2024710d5c97e55f0160661767
David W.H. Swenson <david.swenson@omsf.io>
2025-04-03 10:04:06 -0500

    fix up shift_date_by_months

--- src/spec0/utils/dates.py
+++ src/spec0/utils/dates.py
@@ -32,8 +32,20 @@ def quarter_to_date(quarter):
 
 def shift_date_by_months(date, n_months):
     """Shift a date by a number of months."""
-    # used to set the cutoff; if there's a better way to do this, go for it.
     # Months are weird because they aren't all the same length.
-    dyears = n_months // 12
-    dmonths = n_months % 12
-    return date.replace(year=date.year + dyears, month=date.month + dmonths)
+    total_month = date.month + n_months
+    new_year = date.year + (total_month - 1) // 12
+    new_month = (total_month - 1) % 12 + 1
+
+    try:
+        new_date = date.replace(year=new_year, month=new_month, day=date.day)
+    except ValueError:
+        # If the target month doesn't have the original day (e.g. February 31),
+        # shift to the first day of the next month.
+        if new_month == 12:
+            new_year += 1
+            new_month = 1
+        else:
+            new_month += 1
+        new_date = date.replace(year=new_year, month=new_month, day=1)
+    return new_date
--- tests/utils/test_dates.py
+++ tests/utils/test_dates.py
@@ -37,6 +37,16 @@ def test_quarter_to_date():
     [
         (datetime.datetime(2023, 1, 15), 3, datetime.datetime(2023, 4, 15)),
         (datetime.datetime(2023, 7, 15), 4, datetime.datetime(2023, 11, 15)),
+        (datetime.datetime(2022, 6, 15), 0, datetime.datetime(2022, 6, 15)),
+        (datetime.datetime(2022, 6, 15), 3, datetime.datetime(2022, 9, 15)),
+        (datetime.datetime(2022, 11, 15), 1, datetime.datetime(2022, 12, 15)),
+        (datetime.datetime(2022, 3, 15), 12, datetime.datetime(2023, 3, 15)),
+        (datetime.datetime(2022, 5, 15), 15, datetime.datetime(2023, 8, 15)),
+        (datetime.datetime(2020, 2, 29), 1, datetime.datetime(2020, 3, 29)),
+        (datetime.datetime(2022, 11, 15), 2, datetime.datetime(2023, 1, 15)),
+        (datetime.datetime(2021, 12, 31), 1, datetime.datetime(2022, 1, 31)),
+        (datetime.datetime(2022, 1, 31), 1, datetime.datetime(2022, 3, 1)),
+        (datetime.datetime(2020, 2, 29), 12, datetime.datetime(2021, 3, 1)),
     ],
 )
 def test_shift_date_by_months(datetime, n_months, expected):
